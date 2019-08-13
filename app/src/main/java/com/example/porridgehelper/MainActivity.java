41c65c5a779191429a8dd003f62c47aa16afd0b2
PsPLoG <gkgkgkrj@gmail.com>
2019-08-13 16:21:50 +0900

    Update func

--- app/src/main/java/com/example/porridgehelper/MainActivity.java
+++ app/src/main/java/com/example/porridgehelper/MainActivity.java
@@ -19,8 +19,11 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Double> dataList = new ArrayList<>();
+    ArrayList<Double> dataList_p = new ArrayList<>();
     MediaRecorder recorder;
     AudioRecord audioRecord;
     String filename;
@@ -29,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
     int position = 0; // 다시 시작 기능을 위한 현재 재생 위치 확인 변수
     int time = 0;
     int bufferSize = 0;
+
+    int count = 0;
+    int THRE = 5; // 1-2단계
+    int THRE2 = 10; // 3단계
+    boolean data_type = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,22 +78,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Handler mHandler = new Handler();
+
         findViewById(R.id.record).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 flag = true;
                 startNoiseLevel();
-                final Runnable runnable = new Runnable() {
-                    @Override
-                    public void run() {
 
-                        Log.d("asd", "loading" + getNoiseLevel());
-                        if (flag)
-                            mHandler.postDelayed(this, 1000);
-                    }
-                };
-                mHandler.post(runnable);
             }
         });
 
@@ -100,13 +99,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private double zcr(short[] x, int buffersize) {
-        int z = 0;
+    private double zcr(short[] x, double buffersize) {
+        double z = 0;
         for (int i = 0; i < buffersize - 1; i++) {
             if (x[i] * x[i + 1] < 0)
                 z++;
         }
+        Log.e("ww", "z:" + z + "buffer" + buffersize);
         return z / buffersize;
+    }
+
+    private double power(short[] x, double buffersize) {
+        double z = 0;
+        for (int i = 0; i < buffersize - 1; i++) {
+            x[i] *= x[i];
+        }
+        double avg = 0;
+        for (int i = 0; i < buffersize - 1; i++) {
+            avg += x[i];
+        }
+        avg = avg / buffersize;
+
+        return Math.log(avg);
     }
 
     private void recordAudio() {
@@ -139,46 +153,119 @@ public class MainActivity extends AppCompatActivity {
         if (audioRecord == null)
             audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
                     16000, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+        else if (audioRecord.getState() == AudioRecord.RECORDSTATE_RECORDING) {
+            return;
+        }
+        data_type = true;
         audioRecord.startRecording();
+
+        final Handler mHandler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                //------------------------------------------------------
+                // 죽상태에 따른 처리부분
+                //------------------------------------------------------
+                int state = getNoiseLevel(data_type); // true : power, false : zcr
+
+                if (state == 0) { //음수
+                    count--;
+                } else if (state == 1) { //양수
+                    count++;
+                }
+                if (data_type) {
+                    if (Math.abs(count) >= THRE) {
+                        if (count < 0) { // 양수 --> 음수
+                            Toast.makeText(getApplicationContext(), "2단계", Toast.LENGTH_SHORT).show();
+                            data_type = false;
+                            count = 0;
+                        } else if (count > 0) { // 음수 --> 양수
+                            Toast.makeText(getApplicationContext(), "1단계", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else{
+                    if (Math.abs(count) >= THRE2){
+                        Toast.makeText(getApplicationContext(), "3단계", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                Log.d("sound ", "state" + state);
+                //------------------------------------------------------
+
+                if (flag)
+                    mHandler.postDelayed(this, 1000);
+            }
+        };
+        mHandler.post(runnable);
 
     }
 
-    public double getNoiseLevel() {
+    public int getNoiseLevel(boolean type) {
         short data[] = new short[bufferSize];
         double average = 0.0;
         //recording data;
         audioRecord.read(data, 0, bufferSize);
-        String tmp ="";
-        for(int i=0;i<data.length;i++)
-        {
-            tmp+= data[i]+" ";
+
+
+        dataList.add(zcr(data, bufferSize));
+        dataList_p.add(power(data, bufferSize));
+        int avg = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (i % 100 == 0) {
+                //dataList.add(avg/100);
+                avg = 0;
+            }
+            avg += data[i];
         }
-        Log.d("asd",data.length+"");
-        Log.d("asd",tmp);
-//        for (short s : data) {
-//            if (s > 0) {
-//                average += Math.abs(s);
-//            } else {
-//                bufferSize--;
-//            }
-//        }
-//        //x=max;
-//        double x = average / bufferSize;
-//        Log.e(TAG, "" + x);
-//        double db = 0;
-//        if (x == 0) {
-//            Log.e(TAG, "error x=0");
-//            return db;
-//        }
-//        // calculating the pascal pressure based on the idea that the max amplitude (between 0 and 32767) is
-//        // relative to the pressure
-//        double pressure = x / 51805.5336; //the value 51805.5336 can be derived from asuming that x=32767=0.6325 Pa and x=1 = 0.00002 Pa (the reference value)
-//        Log.d(TAG, "x=" + pressure + " Pa");
-//        db = (20 * Math.log10(pressure / REFERENCE));
-//        Log.d(TAG, "db=" + db);
-//        if (db > 0) {
-//            Log.e(TAG, "error x=0");
-//        }
+        String zcr = "";
+        String power = "";
+        for (int i = dataList.size() - 10; i < dataList.size(); i++) {
+            if (i < 0)
+                continue;
+            zcr += dataList.get(i) + " ";
+            power += dataList_p.get(i) + " ";
+
+        }
+        Log.d("asd", dataList.size() + "");
+        Log.d("sound_zcr__", zcr);
+        Log.d("sound_power", power);
+
+        //----------------------------------------------------------
+        // 죽상태를 반환해주는 코드부분
+        //----------------------------------------------------------
+        double S1 = 0;
+        double B1 = 0;
+
+        if (type) {
+            if (dataList_p.size() < 20)
+                return 0;
+            for (int i = dataList_p.size() - 20; i < dataList_p.size() - 10; i++) {
+                B1 += dataList_p.get(i);           //i번째 시간에서 power값
+                S1 += dataList_p.get(i + 10);
+            }
+
+            double R = S1 - B1;
+            if (R < 0)
+                return 0; // ('---------- PHASE 1 ----------')
+            else if (R > 0) {
+                return 1; // ('---------- PHASE 2 ----------')
+            }
+        } else { // zcr값 사용
+            if (dataList.size() < 20)
+                return 0;
+            for (int i = dataList.size() - 20; i < dataList.size() - 10; i++) {
+                B1 += dataList.get(i);           //i번째 시간에서 power값
+                S1 += dataList.get(i + 10);
+            }
+
+            double R = S1 - B1;
+            if (R < 0)
+                return 0; // ('---------- PHASE 1 ----------')
+            else if (R > 0) {
+                return 1; // ('---------- PHASE 2 ----------')
+            }
+        }
+        //---------------------------------------------------------=
         return 0;
     }
 
